a9f2f94b536839407e9daeac7d535c39ff899bed
David Stancu <dstancu@nyu.edu>
2015-05-30 14:16:06 -0400

    finally working regex can finish rest of app

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,8 @@
+// I realize this is disgusting.
 function inject(key) {
     var r;
-    r = $('.highlight').html().replace(new RegExp('"' + key + '".*?("")', "igm"), '"' + key + '": ' + '"' + $('#payment_' + key).val() + '"');
-    console.log("\"" + key + "\": " + '"' + $('#payment_' + key).val() + '"');
+    r = $('.highlight').html().replace(new RegExp('"'+ key + '".*?".*?".*?".*?".*?".*?".*?', 'igm'), '"' + key + '"</span><span class="p">:</span> <span class="s2">"' + $('#payment_' + key).val() + '"');
+    console.log(r);
     $('.highlight').html(r);
 }
 
@@ -9,6 +10,31 @@ $(document).ready(function() {
   $('#payment_firstName').on({
     keyup: function() {
         inject('firstName');
+    }
+  });
+  $('#payment_lastName').on({
+    keyup: function() {
+        inject('lastName');
+    }
+  });
+  $('#payment_email').on({
+    keyup: function() {
+        inject('email');
+    }
+  });
+  $('#payment_city').on({
+    keyup: function() {
+        inject('city');
+    }
+  });
+  $('#payment_state').on({
+    keyup: function() {
+        inject('state');
+    }
+  });
+  $('#payment_zip').on({
+    keyup: function() {
+        inject('zip');
     }
   });
 });
